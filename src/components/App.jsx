1be592bfacb4b0823886850864504af345811528
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-06-12 18:47:23 +0300

    задача 1 выполена и отправлена на проверку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,21 @@ import Filter from './Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) { this.setState({ contacts }); }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   handleAddContact = (newContact) => {
     const isContactExists = this.state.contacts.some(
@@ -32,7 +39,7 @@ class App extends Component {
     this.setState({ filter: value });
   };
 
-  onDeleteContact = (contactId) => {
+ onDeleteContact = (contactId) => {
     this.setState((prevState) => {
       return {
         contacts: prevState.contacts.filter(
@@ -53,7 +60,7 @@ class App extends Component {
     const { contacts, filter } = this.state;
     const filteredContacts = this.getFiltereContacts();
 
-    return (
+   return (
       <div className="container">
         <h1 className="heading">Phonebook</h1>
         <ContactForm contacts={contacts} onAddContact={this.handleAddContact} />
